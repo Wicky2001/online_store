595233455649bacc6fe12299a189e0150a9c8bc8
Wicky2001 <pmwickramaratna@gmail.com>
2023-08-14 21:50:46 +0530

    add some changes to code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./modules/Home";
 import Footer from "./components/Footer";
 import Product from "./modules/Product";
 import Products from "./modules/Products";
-import AboutUs from "./modules/AboutUs";
+
 import { Routes, Route } from "react-router-dom";
 import CategoryProducts from "./modules/CategoryProduct";
 import Categories from "./components/Categories";
@@ -20,7 +20,6 @@ function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/categories/:name" element={<CategoryProducts />} />
         <Route path="cart" element={<Cart />} />
-        <Route path="/about-us" element={<AboutUs />} />
 
         <Route path="/categories" element={<Categories />} />
 
